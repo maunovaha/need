539a65b503db3059016be62dfb5b575dfc3cccaf
Mauno Vähä <maunovaha@gmail.com>
2017-02-04 21:55:51 +0200

    Replaces jslint with jshint and makes most of the npm tasks to work

---
+++ tasks/header.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var pkg      = require("../package.json")
+  , fs       = require("fs")
+  , path     = require("path")
+  , template = require("underscore.template")
+  , args     = process.argv.slice(2);
+
+if (args.length) {
+  throw new Error("You are doing it wrong, use `node tasks/header.js` by reading stdin!");
+}
+
+function header(data) {
+  var headerFile = fs.readFileSync(path.join(__dirname, "resources", "header.txt"), "utf8")
+    , headerText = template(headerFile, { date: new Date().toISOString(), pkg: pkg });
+  return headerText + data;
+}
+
+var stdin = process.openStdin()
+  , data  = "";
+
+stdin.setEncoding("utf8");
+
+stdin.on("data", function(chunk) {
+  data += chunk;
+});
+
+stdin.on("end", function() {
+  process.stdout.write(header(data));
+});
